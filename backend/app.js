96c1b8f98dc6c35cc61acd513b86aed7070f2459
BlandineBrnt <blandine.breant@gmail.com>
2022-10-31 17:36:20 +0100

    commit 4

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,30 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
 
-app.use((req, res) => {
-    res.json({ message: "Votre requête a bien été reçue !" });
+// Connexion à la base de données mongooseDB
+mongoose
+    .connect(
+        "mongodb+srv://blandine:D8RzhmAe97IMkvY2@cluster0.hubtz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+// Création des en-têtes
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
 });
 
 module.exports = app;
